d386ab882085bfc931024c59b24713a7f220bf6e
Piotr <piot.domzalski91@gmail.com>
2023-04-09 08:39:56 +0200

    Image input

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'Nie zrobiono zdjęcia',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.camera),
+            label: Text('Zrób zdjęcie'),
+            style: TextButton.styleFrom(
+                foregroundColor: Theme.of(context).primaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
