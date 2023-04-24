ad96a116099d5374d10487ab2f49b8d379ab6e41
Piotr <piot.domzalski91@gmail.com>
2023-04-24 14:38:53 +0200

    service and save picture

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
@@ -6,6 +8,20 @@ class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
   List<Place> get items {
-    return [... _items];
+    return [..._items];
+  }
+
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      title: pickedTitle,
+      location: null,
+      image: pickedImage,
+    );
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,8 +2,15 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+
+  ImageInput(this.onSelectImage);
+
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
@@ -14,10 +21,17 @@ class _ImageInputState extends State<ImageInput> {
   // Napiszemy to w metodzie bo będzie trochę kodu, nie potrzebnie zaśmiecać.
   Future<void> _takePicture() async {
     final picker = ImagePicker();
-    final imageFile = await picker.pickImage(
+    final imageFile = await picker.getImage(
       source: ImageSource.camera,
       maxWidth: 600,
     );
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await File(imageFile.path).copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
   }
 
   @override
