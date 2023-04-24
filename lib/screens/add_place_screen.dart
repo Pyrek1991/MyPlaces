b6e64f973daf0c26ac47b07d42a9b2b5b5926ca0
Piotr <piot.domzalski91@gmail.com>
2023-04-24 14:38:41 +0200

    service and save picture

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -13,6 +15,20 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,15 +49,17 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       decoration: InputDecoration(labelText: 'Tytuł'),
                       controller: _titleController,
                     ),
-                    SizedBox(height: 10,),
-                    ImageInput(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
             ),
           ),
           ElevatedButton.icon(
-            onPressed: () {},
+            onPressed: _savePlace,
             icon: Icon(Icons.add),
             label: Text('Dodaj miejsce'),
             //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, // TODO przycisk musi iść na sam dół ekranu,
